357fd7557b2de90beb482a399c8ccbbcc25599fe
suqi <deqi.su@gmail.com>
2017-06-26 12:56:22 +0800

    fix: random kill all has bug, don't release out yet

--- gym_sandbox/__init__.py
+++ gym_sandbox/__init__.py
@@ -70,17 +70,17 @@ register(
     # nondeterministic = True,
 )
 
-register(
-    id='police-1vn-random-killall-ravel-v0',
-    entry_point='gym_sandbox.envs:RandomBallsEnv',
-    timestep_limit=1000,
-
-    kwargs = dict(
-        agent_num=1, agent_team="police",
-        init_thief_num=1, step_add_thief_max=3,
-        adversary_num=100, map_size=100, adversary_action="random",
-        state_format='grid',state_ravel=True,
-    )
-    # reward_threshold=1.0,
-    # nondeterministic = True,
-)
+# register(
+#     id='police-1vn-random-killall-ravel-v0',
+#     entry_point='gym_sandbox.envs:RandomBallsEnv',
+#     timestep_limit=100,
+#
+#     kwargs = dict(
+#         agent_num=1, agent_team="police",
+#         init_thief_num=1, step_add_thief_max=3,
+#         adversary_num=100, map_size=20, adversary_action="random",
+#         state_format='grid',state_ravel=True,
+#     )
+#     # reward_threshold=1.0,
+#     # nondeterministic = True,
+# )
