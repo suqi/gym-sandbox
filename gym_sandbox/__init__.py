3478744b05c949e248acd47804d9cbe8194ff0fa
suqi <deqi.su@gmail.com>
2017-07-04 18:20:06 +0800

    fix: add tips for local_env_list sample

--- gym_sandbox/__init__.py
+++ gym_sandbox/__init__.py
@@ -1,2 +1,5 @@
 from gym_sandbox import std_env_list
-from gym_sandbox import local_env_list
+try:
+    from gym_sandbox import local_env_list
+except:
+    print("Tip: No local env loaded. If you want to diy env, you need local_env_list.py module.")
