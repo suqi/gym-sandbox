1f5c5d459404d18bc575265c0282a8587a270f35
suqi <deqi.su@gmail.com>
2017-07-06 15:40:22 +0800

    feat: Introduce MADDPG env beta!!!

--- gym_sandbox/envs/police_MADDPG.py
+++ gym_sandbox/envs/police_MADDPG.py
@@ -8,11 +8,54 @@ from .police_kill_one import PoliceKillOneEnv
 from gym_sandbox.envs.plot import balls_game_dashboard
 
 
-class MAPoliceKillOneEnv(PoliceKillOneEnv):
+class PoliceMADDPGEnv(PoliceKillOneEnv):
     """
     This is a reproduction env of MADDPG
+    Main difference with Single Agent Env is that: action is a list, and state is a list
+    Observation: a list of relative cord of each police
+    Action: a continous angle(0~2pi)
     DDPG: require env accept a continous action
     MA: step() accept multi action, and return multi result
-    Attention: when any thief is caught, game end!  So this env is suitable for state of fixed postion list
+    KillOne: when any thief is caught, game end!  So this env is suitable for state of fixed postion list
     """
-    pass
+    def __init__(self, **kwargs):
+        # assert kwargs["agent_num"] > 1  # come on, this is MA!
+        assert kwargs["state_format"] == "cord_list_unfixed"  # lower complexity, only fixed cord
+        super().__init__(**kwargs)
+
+    def calc_dist(self, pos1, pos2):
+        """as action is a continous angle with any direction, now dist should be radius based"""
+        _coords1 = np.array(pos1)
+        _coords2 = np.array(pos2)
+
+        # calc Euclidean Distance
+        # alternative way: np.linalg.norm(_coords1 - _coords2)
+        eucl_dist = np.sqrt(np.sum((_coords1 - _coords2) ** 2))
+        return eucl_dist
+
+    def police_move(self, police_list, police_actions):
+        # Accept continous move action, which is more suitable for MADDPG
+        police_actions = np.clip(police_actions, 0, np.pi * 2)
+
+        police_new_loc = police_list.copy()
+        police_speed = self.teams['police']['speed']
+        for _i, _a in enumerate(police_actions):
+            action_dir = np.array([np.cos(_a), np.sin(_a)])
+            police_dir = action_dir * police_speed
+            _p = police_list[_i]
+            _p = (_p[0] + police_dir[0], _p[1] + police_dir[1])
+            _p = self.ensure_inside(_p)
+            police_new_loc[_i] = _p
+
+        return police_new_loc
+
+    def _trans_state(self, state):
+        # now only support cord_list_unfixed, so must be KillOne mode!
+        # Firstly absolute cord
+        abs_ob = [_p for _p in state["police"]] + [_t for _t in state["thief"]]
+        abs_ob = np.array(abs_ob)
+
+        # now relative cord (make self position as (0,0))
+        relative_state = [abs_ob.copy() - np.array(_p) for _p in state["police"]]
+
+        return np.array(relative_state) / self.map_size
--- test/demo_run_env.py
+++ test/demo_run_env.py
@@ -7,9 +7,10 @@ import time
 # env = gym.make("police-killall-ravel-v0")
 # env = gym.make("police-killall-static-cords-500-v0")
 # env = gym.make("police-killall-trigger-3dravel-v0")
-env = gym.make("police-bokeh-server-v0")
+# env = gym.make("police-bokeh-server-v0")
+env = gym.make("police-maddpg-v0")
 
-env.env.init_params(show_dashboard=True)
+env.env.init_params(show_dashboard=True, bokeh_output="standalone")
 
 print("action shape >>> ", env.action_space.n)
 
@@ -20,6 +21,7 @@ for i in range(10):
     # choose your action
     a = 1
     s_, r, done, info = env.step(a)
+    env.render()
     time.sleep(10)
     print(s_, r, done, info)
     if done:
