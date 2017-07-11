81631343953846f97f8ad4751b50446021d6e836
suqi <deqi.su@gmail.com>
2017-07-11 15:13:01 +0800

    feat: CommNet to solve MA!

---
+++ test_algos/MADDPG_Morvan/CommNet_DDPG_police.py
@@ -0,0 +1,173 @@
+"""
+CommNet solution to solve MA
+ https://arxiv.org/abs/1605.07736
+"""
+
+import time
+import tensorflow as tf
+import numpy as np
+import gym
+import gym_sandbox
+
+np.random.seed(1)
+tf.set_random_seed(1)
+
+#####################  hyper parameters  ####################
+
+MAX_EPISODES = 7000
+MAX_EP_STEPS = 100
+LR_A = 0.01  # learning rate for actor
+LR_C = 0.01  # learning rate for critic
+GAMMA = 0.9  # reward discount
+REPLACE_ITER_A = 500
+REPLACE_ITER_C = 300
+MEMORY_CAPACITY = 2000  # memory bigger, performance better, must be a big memory!
+BATCH_SIZE = 32
+
+var = 3  # control exploration, this is for make some noise, but must be small when already converge
+var_decay = .99995  # decay the action randomness
+
+RENDER = True
+RENDER_FPS = 10
+
+ENV_NAME = 'police-commnet-continous-2agent-v0'
+AGENT_NUM = 2
+
+###############################  Actor  ####################################
+
+
+class DDPG(object):
+    def __init__(self, a_dim, s_dim, a_bound,):
+        self.memory = np.zeros((MEMORY_CAPACITY, s_dim * 2 + a_dim + 1), dtype=np.float32)
+        self.pointer = 0
+        self.sess = tf.Session()
+        self.a_replace_counter, self.c_replace_counter = 0, 0
+
+        self.a_dim, self.s_dim, self.a_bound = a_dim, s_dim, a_bound,
+        self.S = tf.placeholder(tf.float32, [None, s_dim], 's')
+        self.S_ = tf.placeholder(tf.float32, [None, s_dim], 's_')
+        self.R = tf.placeholder(tf.float32, [None, 1], 'r')
+
+        with tf.variable_scope('Actor'):
+            self.a = self._build_a(self.S, scope='eval', trainable=True)
+            a_ = self._build_a(self.S_, scope='target', trainable=False)
+        with tf.variable_scope('Critic'):
+            # assign self.a = a in memory when calculating q for td_error,
+            # otherwise the self.a is from Actor when updating Actor
+            q = self._build_c(self.S, self.a, scope='eval', trainable=True)
+            q_ = self._build_c(self.S_, a_, scope='target', trainable=False)
+
+        # networks parameters
+        self.ae_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Actor/eval')
+        self.at_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Actor/target')
+        self.ce_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/eval')
+        self.ct_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/target')
+
+        q_target = self.R + GAMMA * q_
+        # in the feed_dic for the td_error, the self.a should change to actions in memory
+        td_error = tf.losses.mean_squared_error(labels=q_target, predictions=q)
+        self.ctrain = tf.train.AdamOptimizer(LR_C).minimize(td_error, var_list=self.ce_params)
+
+        a_loss = - tf.reduce_mean(q)    # maximize the q
+        self.atrain = tf.train.AdamOptimizer(LR_A).minimize(a_loss, var_list=self.ae_params)
+
+        self.sess.run(tf.global_variables_initializer())
+
+    def choose_action(self, s):
+        return self.sess.run(self.a, {self.S: s[np.newaxis, :]})[0]
+
+    def learn(self):
+        # hard replace parameters
+        if self.a_replace_counter % REPLACE_ITER_A == 0:
+            self.sess.run([tf.assign(t, e) for t, e in zip(self.at_params, self.ae_params)])
+        if self.c_replace_counter % REPLACE_ITER_C == 0:
+            self.sess.run([tf.assign(t, e) for t, e in zip(self.ct_params, self.ce_params)])
+        self.a_replace_counter += 1; self.c_replace_counter += 1
+
+        indices = np.random.choice(MEMORY_CAPACITY, size=BATCH_SIZE)
+        bt = self.memory[indices, :]
+        bs = bt[:, :self.s_dim]
+        ba = bt[:, self.s_dim: self.s_dim + self.a_dim]
+        br = bt[:, -self.s_dim - 1: -self.s_dim]
+        bs_ = bt[:, -self.s_dim:]
+
+        self.sess.run(self.atrain, {self.S: bs})
+        self.sess.run(self.ctrain, {self.S: bs, self.a: ba, self.R: br, self.S_: bs_})
+
+    def store_transition(self, s, a, r, s_):
+        transition = np.hstack((s, a, [r], s_))
+        index = self.pointer % MEMORY_CAPACITY  # replace the old memory with new memory
+        self.memory[index, :] = transition
+        self.pointer += 1
+
+    def _build_a(self, s, scope, trainable):
+        with tf.variable_scope(scope):
+            net = tf.layers.dense(s, 200, activation=tf.nn.relu, name='l1', trainable=trainable)
+            a = tf.layers.dense(net, self.a_dim, activation=tf.nn.tanh, name='a', trainable=trainable)
+            return tf.multiply(a, self.a_bound, name='scaled_a')
+
+    def _build_c(self, s, a, scope, trainable):
+        with tf.variable_scope(scope):
+            n_l1 = 100
+            w1_s = tf.get_variable('w1_s', [self.s_dim, n_l1], trainable=trainable)
+            w1_a = tf.get_variable('w1_a', [self.a_dim, n_l1], trainable=trainable)
+            b1 = tf.get_variable('b1', [1, n_l1], trainable=trainable)
+            net = tf.nn.relu(tf.matmul(s, w1_s) + tf.matmul(a, w1_a) + b1)
+            return tf.layers.dense(net, 1, trainable=trainable)  # Q(s,a)
+
+
+
+
+
+
+
+# ----------- main entry ----------------
+
+env = gym.make(ENV_NAME)
+if RENDER:
+    env.env.init_params(show_dashboard=True, bokeh_output='standalone')
+# env = env.unwrapped
+env.seed(1)
+
+s_dim = env.observation_space.shape[0]
+a_dim = env.action_space.shape[0] * AGENT_NUM
+a_bound = env.action_space.high
+
+ddpg = DDPG(a_dim, s_dim, a_bound)
+saver = tf.train.Saver()
+# saver.restore(ddpg.sess, '.tf-models/commnet-ddpg-1000')
+
+for i in range(MAX_EPISODES):
+    s = env.reset()
+    ep_reward = 0
+
+    if i > 1 and i % 1000 == 0:
+        saver.save(ddpg.sess, ".tf-models/commnet-ddpg", global_step=i)
+
+    for j in range(MAX_EP_STEPS):
+        # Add exploration noise
+        a = ddpg.choose_action(s)
+
+        # this is a noise-like implementation
+        a = np.clip(np.random.normal(a, var), -a_bound, a_bound)    # add randomness to action selection for exploration
+
+        s_, r, done, info = env.step(a)
+        if RENDER:
+            env.render()
+            time.sleep(1.0/RENDER_FPS)
+
+        ddpg.store_transition(s, a, r / 10, s_)
+
+        if ddpg.pointer > MEMORY_CAPACITY:
+            var *= var_decay
+            ddpg.learn()
+
+        s = s_
+        ep_reward += r
+
+        if done:
+            if RENDER:
+                time.sleep(1.0/RENDER_FPS)
+            print('Episode:', i, ' Reward: %i' % int(ep_reward), 'Explore: %.2f' % var, )
+            # if ep_reward > -1000:RENDER = True
+            break
